02f26c459944c6cc0e633fdcdfa319391e28bd17
HeeJae Seol <gnwo10@naver.com>
2025-07-06 00:01:02 +0900

    10871_Num.py

---
+++ Bronze/10871_Num.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+N,X = map(int,input().split())
+nums = list(map(int,input().split()))
+
+for num in nums:
+    if num < X:
+        print(num, end=' ')
+
+    
